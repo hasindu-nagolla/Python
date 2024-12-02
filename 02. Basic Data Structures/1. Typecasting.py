998d94c8e040573dee739ff8c2465f6e7705e18d
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-12-02 20:17:07 +0530

    Typecasting

--- 02. Basic Data Structures/1. Typecasting.py
+++ 02. Basic Data Structures/1. Typecasting.py
@@ -28,4 +28,37 @@ num_int = int(num_str)  # int() function converts string to int
 print("String:", num_str)
 print("Integer:", num_int)
 
-#
+# Converting float to string
+
+num_float = 10.5
+num_str = str(num_float)  # str() function converts float to string
+print("Float:", num_float)
+print("String:", num_str, type(num_str))
+
+# Converting string to float
+
+num_str = "10.5"
+num_float = float(num_str)  # float() function converts string to float
+print("String:", num_str)
+print("Float:", num_float, type(num_float))
+
+# Converting string to list
+
+num_str = "10, 20, 30, 40, 50"
+num_list = num_str.split(", ")  # split() function converts string to list
+print("String:", num_str)
+print("List:", num_list, type(num_list))
+
+# Converting list to string
+
+num_list = ["10", "20", "30", "40", "50"]
+num_str = ", ".join(num_list)  # join() function converts list to string
+print("List:", num_list)
+print("String:", num_str, type(num_str))
+
+# We can perform with other data types as well. also data structures like tuple, set, dictionary, etc.
+
+
+
+
+    
