cbe02789d5cab24b95ba4d718dbb9e4b221a96b8
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-12-25 07:25:54 +0530

    dictionaries

--- 02. Basic Data Structures/4. Dictionaries.py
+++ 02. Basic Data Structures/4. Dictionaries.py
@@ -1,15 +1,23 @@
 # Dictionaries is a collection of key-value pairs.
 
 # Create a dictionary.
-myDictioanry = {'1000': "Matale", '2000': "Kandy",
-                '3000': "Colombo", '4000': "Galle"}
+myDictioanry = {'1000': "Matale",
+                '2000': "Kandy",
+                '3000': "Colombo",
+                '4000': "Galle",
+                '4000': "Matara"}  # If we add a key-value pair with the same key, it will replace the existing value.
 
-# Add a new key-value pair to the dictionary.
-myDictioanry['1200'] = "Kurunegala"
+newDict = dict(name = "Hasindu", age = 23)  # Create a dictionary using dict() function.
+print(newDict)
+
+myDictioanry['1200'] = "Kurunegala" # Add a new key-value pair to the dictionary.
 
 # Add a new key-value pair to the dictionary. we can add any type of key and value. this is integer key.
 myDictioanry[10] = "Anuradhapura"
 print(myDictioanry)
+
+print(len(myDictioanry))  # Get the length of the dictionary.
+print(type(myDictioanry))  # Get the type of the dictionary.
 
 # Access the value of the key '1000'. we cannot use index to access the value.
 y = myDictioanry['1000']
@@ -62,12 +70,21 @@ xDic = {
     "a": ['Hello', 'Hi', 'Hey'],
     "b": ('Apple', 'Banana', 'Cherry'),
 }
+print(xDic)
 
 getY = xDic['a']  # Get the value of the key 'a'.
 print(getY)
 
 getY.append('Hola')  # Add 'Hola' to the list.
 print(getY)
+
+xDic['b'] = ('Apple', 'Banana', 'Cherry', 'Mango')  # Change the value of the key 'b'.
+print(xDic)
+
+xDic.update({'b': 2024})  # Update the value of the key 'b'.
+print(xDic)
+
+
 
 # see it, you do anything to y, it will affect the dictionary. but it will not affect if "a" is a basic data type. like integer.
 print(xDic)
@@ -93,3 +110,19 @@ europe['sri lanka'] = data
 
 # print europe dictionary.
 print(europe)
+
+# Loop Dictionaries in Python.
+
+numbersDictioanry = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five'}
+
+for x in numbersDictioanry:
+    print(x)  # Print the keys of the dictionary.
+    
+for x in numbersDictioanry:
+    print(numbersDictioanry[x])  # Print the values of the dictionary.
+
+for x in numbersDictioanry.values():
+    print(x)  # Also can use .values() to get the values of the dictionary.
+    
+for x, y in numbersDictioanry.items():
+    print(x, y)  # Print the both key-value pairs of the dictionary.
