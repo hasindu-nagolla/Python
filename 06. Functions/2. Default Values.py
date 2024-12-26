434af3e2bfd9181f4ed51b46ca73416a236fc1d1
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-12-26 17:45:49 +0530

    Function with Default Values

--- 06. Functions/2. Default Values.py
+++ 06. Functions/2. Default Values.py
@@ -1,5 +1,10 @@
-"""Default values in Python function is used to set the default value of the argument.
-If the value of the argument is not passed to the function then the default value is used."""
+# Function with Default Values
+
+# ==================================================================================
+# Default values in Python function is used to set the default value of the argument. 
+# If the value of the argument is not passed to the function then the default value
+# is used.
+# ==================================================================================
 
 
 def myFunction(marks, subject="Unknown"):  # Default value of subject is "Unknown"
@@ -21,8 +26,21 @@ def myFunction(marks, subject="Unknown"):  # Default value of subject is "Unknow
 
 # Calling the function with the subject name. in this method we can pass only one argument. because we have set a default value for the subject.
 myFunction(45)
+
 # Calling the function with subject name and marks. It's called a named argument. we can change the default value of the subject.
 myFunction(marks=75, subject="Science")
 
-"""we cannot pass the value of subject without passing the value of marks. because we have not set the default value of marks. 
-also, we cannot create a parameter without a default value after the parameter with a default value."""
+
+# ===========================================================================================================
+# Note: we cannot pass the value of subject without passing the value of marks. because we have not set the 
+# default value of marks. also, we cannot create a parameter without a default value after the parameter 
+# with a default value.
+# ===========================================================================================================
+
+
+# ========================================================
+# Author: Hasindu Nagolla
+# Copyright © 2024 Hasindu Nagolla
+# LinkedIn: https://www.linkedin.com/in/hasindume/
+# ========================================================
+
