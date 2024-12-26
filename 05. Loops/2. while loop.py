b5731e842a5ab73e97ae9a57d5264d20c54a9128
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-12-26 22:17:01 +0530

    for loop

--- 05. Loops/2. while loop.py
+++ 05. Loops/2. while loop.py
@@ -1,4 +1,8 @@
-# While Loop in Python is used to execute a block of statements repeatedly until a given condition is satisfied. And when the condition becomes false, the line immediately after the loop in the program is executed.
+# ========================================================
+# Section 1: While Loop
+# ========================================================
+# While Loop in Python is used to execute a block of statements repeatedly until a given condition is satisfied. 
+# And when the condition becomes false, the line immediately after the loop in the program is executed.
 
 x = [1, 2, 3, 4, 5]  # List
 
@@ -7,3 +11,10 @@ count = 0
 while count <= 10:  # Loop will stop when count is greater than 10
     print("Count is: ", count)
     count += 1
+
+
+# ========================================================
+# Author: Hasindu Nagolla
+# Copyright © 2024 Hasindu Nagolla
+# LinkedIn: https://www.linkedin.com/in/hasindume/
+# ========================================================
