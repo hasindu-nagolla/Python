333ff2e7fbbb02e777f85d8973f0bec702a15ac3
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2024-12-27 07:00:19 +0530

    Modules

---
+++ 11. Modules/02. CreateModule.py
@@ -0,0 +1,22 @@
+# Create Module
+
+# ========================================================
+# Section 1: Defining the Module
+# ========================================================
+
+name = "user 01"
+
+def get_name():
+    print(__name__)  # This will print the name of the module.
+    return name
+
+def get_age():
+    return 20
+
+
+
+# ========================================================
+# Author: Hasindu Nagolla
+# Copyright © 2024 Hasindu Nagolla
+# LinkedIn: https://www.linkedin.com/in/hasindume/
+# ========================================================
