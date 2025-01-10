d38ed2c15c02970cb44743d3bbae522e00325af8
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2025-01-10 08:55:49 +0530

    typecasting

--- 02. Basic Data Structures/1. Typecasting.py
+++ 02. Basic Data Structures/1. Typecasting.py
@@ -71,5 +71,5 @@ print("String:", num_str, type(num_str))
 # ========================================================
 # Author: Hasindu Nagolla
 # Copyright © 2024 Hasindu Nagolla
-# LinkedIn: https://www.linkedin.com/in/hasindume/
+# LinkedIn: https://www.linkedin.com/in/hasindu-nagolla/
 # ========================================================
