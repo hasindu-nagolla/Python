0236e34810e3649d3e580afaf61dd015a7365d91
Hasindu Lakshan <hasindulakshan0040@gmail.com>
2025-01-14 06:42:23 +0530

    dependencies

--- 12. Dependencies/Example.py
+++ 12. Dependencies/Example.py
@@ -28,5 +28,5 @@ print(response.json())  # This will print the response in JSON format
 # ========================================================
 # Author: Hasindu Nagolla
 # Copyright © 2024 Hasindu Nagolla
-# LinkedIn: https://www.linkedin.com/in/hasindume/
+# LinkedIn: https://www.linkedin.com/in/hasindu-nagolla/
 # ========================================================
